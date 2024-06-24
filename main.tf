8b9075553b42ebe539a2514064f8333a00afbd2e
Sangho Kim <sk80@indiana.edu>
2024-06-24 15:25:41 -0400

    removed target_id

--- main.tf
+++ main.tf
@@ -103,7 +103,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
