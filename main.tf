e49dec27e1ce29fa42edb13290080b26bc3c5336
Sangho Kim <sk80@indiana.edu>
2024-06-24 15:22:45 -0400

    autoscailing

--- main.tf
+++ main.tf
@@ -34,19 +34,24 @@ module "blog_vpc" {
   }
 }
 
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.6.1"
+  # insert the 1 required variable here
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -63,7 +68,7 @@ module "blog_sg" {
 
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
